9417bee034f807455f6584def8309767a33e7274
Hank Kung <hank910140@gmail.com>
2022-02-05 20:46:21 +0800

    update maskrcnn weights

--- models/maskrcnn.py
+++ models/maskrcnn.py
@@ -13,25 +13,32 @@ def get_maskrcnn():
 	cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
 	# cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
 	cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+	path = cfg.MODEL.WEIGHTS
 	model = build_model(cfg)
+	DetectionCheckpointer(model).load(path)
 	return model
 
 
 # test
 
-loader = transforms.Compose([
-    transforms.ToTensor()])
+# loader = transforms.Compose([
+#     transforms.ToTensor()])
 
-# im = cv2.imread("1.jpg")
-im = Image.open('1.jpg').convert('RGB')
-# im = Image.fromarray(im)
-img = loader(im).unsqueeze(0)
-im = img.to('cuda', torch.float)
+# # im = cv2.imread("1.jpg")
+# im = Image.open('1.jpg').convert('RGB')
+# # im = Image.fromarray(im)
+# img = loader(im).unsqueeze(0)
+# im = img.to('cuda', torch.float)
 # print(im.shape)
 # im = ImageList.from_tensors([im.cuda()])
-# tensor = torch.ones([1, 3, 224, 224], dtype=torch.float32).cuda()
+tensor = torch.ones([1, 3, 224, 224], dtype=torch.float32).cuda()
 model = get_maskrcnn().cuda()
 model.eval()
-features = model.backbone(im)
-proposals, _ = model.proposal_generator(im, features)
-instances, _ = model.roi_heads(im, features, proposals)
+features = model.backbone(tensor)
+# for k, v in features.items():
+# 	print(k)
+print(features['p6'][0][0][0][0:3])
+# print(len(features))
+
+# proposals, _ = model.proposal_generator(im, features)
+# instances, _ = model.roi_heads(im, features, proposals)
