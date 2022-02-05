00c80972633fa369cfdffce86319f5f2bc0090fc
Hank Kung <hank910140@gmail.com>
2022-02-05 22:14:31 +0800

    update feature extractor

---
+++ models/backbone.py
@@ -0,0 +1,42 @@
+from detectron2.modeling import build_model
+from detectron2 import model_zoo
+from detectron2.checkpoint import DetectionCheckpointer
+from detectron2.config import get_cfg
+from detectron2.structures.image_list import ImageList
+import torch
+from torchvision import transforms
+import cv2
+from PIL import Image
+
+def get_maskrcnn():
+    cfg = get_cfg()
+    cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+    # cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+    path = cfg.MODEL.WEIGHTS
+    model = build_model(cfg)
+    DetectionCheckpointer(model).load(path)
+    return model.backbone.bottom_up
+
+
+# test
+
+# loader = transforms.Compose([
+#     transforms.ToTensor()])
+
+# # im = cv2.imread("1.jpg")
+# im = Image.open('1.jpg').convert('RGB')
+# # im = Image.fromarray(im)
+# img = loader(im).unsqueeze(0)
+# im = img.to('cuda', torch.float)
+# print(im.shape)
+# im = ImageList.from_tensors([im.cuda()])
+tensor = torch.ones([1, 3, 224, 224], dtype=torch.float32).cuda()
+model = get_maskrcnn().cuda()
+# print(model.backbone.__dict__)
+
+model.eval()
+
+# ResNet features
+features = model(tensor)
+print(features['res5'].shape)
