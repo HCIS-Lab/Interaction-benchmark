531e97ab54c334c6ddbf66cf49313e248ac0d409
nargoo0328 <71312126+nargoo0328@users.noreply.github.com>
2022-06-09 10:56:33 +0800

    Update features_extraction_carla.py

--- models/features_extraction_carla.py
+++ models/features_extraction_carla.py
@@ -26,25 +26,25 @@ def get_features(root_path, object_per_frame, batch_n):
     scenario_key = ['collision','interactive','non-interactive']
     curr_path = root_path
     path = [curr_path]
+    count = 0
     for scenario_type in os.listdir(curr_path):
         if scenario_type in scenario_key:
             curr_path = path[-1]
             curr_path = os.path.join(curr_path,scenario_type)
             path.append(curr_path)
             for scenario_id in os.listdir(curr_path):
+                if count == 8:
+                    return
+                count+=1
                 print(scenario_id)
                 curr_path = path[-1]
                 curr_path = os.path.join(curr_path,scenario_id,'variant_scenario')
                 path.append(curr_path)
                 for variant_name in os.listdir(curr_path):
                     curr_path = path[-1]
-                    print("\t",variant_name)
                     curr_path = os.path.join(curr_path,variant_name)
                     if not os.path.isdir(os.path.join(curr_path,'features')):
                         os.mkdir(os.path.join(curr_path,'features'))
-                    # Init data
-                    roi_tensor = None
-                    bbox_tensor = None
                     # Read rgb
                     img_archive = zipfile.ZipFile(os.path.join(curr_path,'rgb','front.zip'), 'r')
                     zip_file_list = img_archive.namelist()
@@ -53,17 +53,27 @@ def get_features(root_path, object_per_frame, batch_n):
                     bbox_archive = zipfile.ZipFile(os.path.join(curr_path,'bbox','front.zip'), 'r')
                     zip_file_list = bbox_archive.namelist()
                     bbox_file_list = sorted(zip_file_list)[2:]
+                    # Read collision history
+                    json_file = open(os.path.join(curr_path,'collision_history.json'),'r')
+                    history = json.loads(json_file.read())
+                    json_file.close()
+                    collision_frame = history[0]['frame']
                     # Align frame number
                     index = -1
-                    while img_file_list[-1][-7:-4]!=bbox_file_list[index][-8:-5]:
+                    while img_file_list[index][-7:-4]!=bbox_file_list[-1][-8:-5]:
                         index -= 1
                     if index!=-1:
-                        bbox_file_list = bbox_file_list[:,index+1]
+                        img_file_list = img_file_list[:index+1]
                     # If video length < 100
-                    if len(bbox_file_list)<100:
+                    if len(bbox_file_list)<100 or len(img_file_list)<100:
                         continue
+                    print("\t",variant_name)
                     img_file_list = img_file_list[-100:]
                     bbox_file_list = bbox_file_list[-100:]
+                    # Init data
+                    features_tensor = None
+                    roi_tensor = None
+                    bbox_tensor = None
                     i = 0
                     while i < 100:
                         inputs = []
@@ -80,55 +90,53 @@ def get_features(root_path, object_per_frame, batch_n):
                             frame = torch.as_tensor(frame.astype("float32").transpose(2, 0, 1))
                             inputs.append({"image": frame, "height": height, "width": width})
                             i += 1
-                        roi_batch, bbox_batch = run_model([maskformer,model],inputs,batch_n)
+                        featrues_batch, roi_batch, bbox_batch = run_model([maskformer,model],inputs,batch_n)
                         if roi_tensor is None:
+                            features_tensor = featrues_batch
                             roi_tensor = roi_batch
                             bbox_tensor = bbox_batch
                         else:
+                            features_tensor = torch.cat((features_tensor,featrues_batch))
                             roi_tensor = torch.cat((roi_tensor,roi_batch))
                             bbox_tensor = torch.cat((bbox_tensor,bbox_batch))
-                    # print(roi_batch.shape)
-                    # print(bbox_tensor.shape)
+                    torch.save(features_tensor,os.path.join(curr_path,'features','features.pt'))
                     torch.save(roi_tensor,os.path.join(curr_path,'features','roi.pt'))
                     torch.save(bbox_tensor,os.path.join(curr_path,'features','bbox.pt'))
-                    return
                 path.pop()
             path.pop()
             return
 
 def run_model(models, inputs, batch_n):
     maskformer, model = models
-    images = model.preprocess_image(inputs)
-    fpn_features = maskformer.get_fpn_features(inputs)
-    # features_maskformer = fpn_features[3]
-    # roi align
-    features = model.backbone(images.tensor)  # set of cnn features
-    proposals, _ = model.proposal_generator(images, features, None)  # RPN
-    features_ = [features[f] for f in model.roi_heads.in_features]
-    # ROI ALIGN
-    box_features = model.roi_heads.box_pooler(features_, [x.proposal_boxes for x in proposals])
-    # Flatten roi align features
-    box_features = model.roi_heads.box_head(box_features)  # features of all 1k candidates
-    predictions = model.roi_heads.box_predictor(box_features)
-    pred_instances, _ = model.roi_heads.box_predictor.inference(predictions, proposals)
-    pred_instances = model.roi_heads.forward_with_given_boxes(features, pred_instances)
-    # output boxes, masks, scores, etc
-    pred_instances = model._postprocess(pred_instances, inputs, images.image_sizes)  # scale box to orig size
-    roi_input = []
-    for i in range(batch_n):
-        ins = pred_instances[i]["instances"]
-        size = pred_instances[i]['instances'].scores.size(dim=0)
-        if size>20:
-            roi_input.append(ins.pred_boxes[:20])
-        else:
-            temp = Boxes(torch.zeros(20-size,4).cuda())
-            pred_boxes = Boxes.cat([ins.pred_boxes,temp])
-            roi_input.append(pred_boxes)
-    roi = model.roi_heads.box_pooler(fpn_features[1:],roi_input).view(batch_n,20,-1)
-    return roi, Boxes.cat(roi_input).tensor.view(batch_n,20,-1)
-    # batch_data_flat[:,frame_i*img_per_batch:(frame_i+1)*img_per_batch] = roi.cpu().numpy()
-    # features_maskformer = features_maskformer.unsqueeze(0).view(1,batch_n,-1)
-    # batch_data[:,frame_i*img_per_batch:(frame_i+1)*img_per_batch] = features_maskformer.cpu().numpy()
+    with torch.no_grad():
+        images = model.preprocess_image(inputs)
+        fpn_features = maskformer.get_fpn_features(inputs)
+        features_maskformer = fpn_features[4]
+        # roi align
+        features = model.backbone(images.tensor)  # set of cnn features
+        proposals, _ = model.proposal_generator(images, features, None)  # RPN
+        features_ = [features[f] for f in model.roi_heads.in_features]
+        # ROI ALIGN
+        box_features = model.roi_heads.box_pooler(features_, [x.proposal_boxes for x in proposals])
+        # Flatten roi align features
+        box_features = model.roi_heads.box_head(box_features)  # features of all 1k candidates
+        predictions = model.roi_heads.box_predictor(box_features)
+        pred_instances, _ = model.roi_heads.box_predictor.inference(predictions, proposals)
+        pred_instances = model.roi_heads.forward_with_given_boxes(features, pred_instances)
+        # output boxes, masks, scores, etc
+        pred_instances = model._postprocess(pred_instances, inputs, images.image_sizes)  # scale box to orig size
+        roi_input = []
+        for i in range(batch_n):
+            ins = pred_instances[i]["instances"]
+            size = pred_instances[i]['instances'].scores.size(dim=0)
+            if size>20:
+                roi_input.append(ins.pred_boxes[:20])
+            else:
+                temp = Boxes(torch.zeros(20-size,4).cuda())
+                pred_boxes = Boxes.cat([ins.pred_boxes,temp])
+                roi_input.append(pred_boxes)
+        roi = model.roi_heads.box_pooler(fpn_features[1:],roi_input).view(batch_n,20,-1)
+        return features_maskformer.cpu(),roi.cpu(), Boxes.cat(roi_input).tensor.view(batch_n,20,-1).cpu()
 
 # class CARLA_IMG_FEATURE():
 if __name__ == '__main__':
