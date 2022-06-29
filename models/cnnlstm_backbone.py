cd88fc54278f2e71fd075af983b96ec8542bcc83
Hank Kung <hank910140@gmail.com>
2022-06-29 22:49:44 +0800

    multi-view

--- models/cnnlstm_backbone.py
+++ models/cnnlstm_backbone.py
@@ -7,7 +7,6 @@ from MaskFormer.demo.demo import get_maskformer
 from retrieval_head import Head, Road_Head
 
 
-
 class CNNLSTM_maskformer(nn.Module):
     def __init__(self, num_cam, num_ego_class, num_actor_class, road):
         super(CNNLSTM_maskformer, self).__init__()
@@ -16,19 +15,25 @@ class CNNLSTM_maskformer(nn.Module):
         self.backbone = get_maskformer()
 
         self.conv1 = nn.Sequential(
-                nn.ReLU(inplace=True),
-                nn.Conv2d(2048, 1536, kernel_size=3, stride=1, padding='same'),
-                nn.BatchNorm2d(1536),
-                nn.ReLU(inplace=True),
-                nn.Conv2d(1536, 1024, kernel_size=3, stride=1, padding='same'),
+                nn.ReLU(inplace=False),
+                nn.Conv2d(2048*self.num_cam, 1024*self.num_cam, kernel_size=1, stride=1, padding='same'),
+                nn.BatchNorm2d(1024*self.num_cam),
+                nn.ReLU(inplace=False),
+                nn.Conv2d(1024*self.num_cam, 1024, kernel_size=1, stride=1, padding='same'),
                 nn.BatchNorm2d(1024),
+                nn.ReLU(inplace=False),
+                nn.Conv2d(1024, 512, kernel_size=1, stride=1, padding='same'),
+                nn.BatchNorm2d(512)
+                    )
+
+        self.conv2 = nn.Sequential(
                 nn.ReLU(inplace=True),
-                nn.Conv2d(1024, 512, kernel_size=3, stride=1, padding='same'),
-                nn.BatchNorm2d(512),
+                nn.Conv2d(512, 400, kernel_size=3, stride=1, padding='same'),
+                nn.BatchNorm2d(400),
                 nn.ReLU(inplace=True),
-                nn.Conv2d(512*self.num_cam, 256, kernel_size=1, stride=1, padding='same'),
+                nn.Conv2d(400, 256, kernel_size=3, stride=1, padding='same'),
                 nn.BatchNorm2d(256),
-                nn.ReLU(inplace=True),
+                nn.ReLU(inplace=True)
                     )
 
         self.en_lstm = nn.LSTM(input_size=256, hidden_size=256, num_layers=1, batch_first=True)
@@ -40,18 +45,13 @@ class CNNLSTM_maskformer(nn.Module):
         if self.road:
             self.road_enc = nn.Sequential(
                     nn.ReLU(inplace=False),
-                    nn.Conv2d(2048, 1536, kernel_size=3, stride=1, padding='same'),
-                    nn.BatchNorm2d(1536),
+                    nn.Conv2d(512, 400, kernel_size=3, stride=1, padding='same'),
+                    nn.BatchNorm2d(400),
                     nn.ReLU(inplace=False),
-                    nn.Conv2d(1536, 1024, kernel_size=3, stride=1, padding='same'),
-                    nn.BatchNorm2d(1024),
-                    nn.ReLU(inplace=False),
-                    nn.Conv2d(1024, 512, kernel_size=3, stride=1, padding='same'),
-                    nn.BatchNorm2d(512),
-                    nn.ReLU(inplace=False),
-                    nn.Conv2d(512*self.num_cam, 256, kernel_size=1, stride=1, padding='same'),
-                    nn.BatchNorm2d(256),
+                    nn.Conv2d(400, 256, kernel_size=3, stride=1, padding='same'),
+                    nn.BatchNorm2d(256)
                         )
+
             self.road_fc = Road_Head(256)
             self.fusion = nn.Sequential(
                 nn.ReLU(inplace=False),
@@ -69,16 +69,24 @@ class CNNLSTM_maskformer(nn.Module):
         seq_len = len(x)//self.num_cam
         batch_size = x[0].shape[0]
 
-        w, h = x[0].shape[2], x[0].shape[3]
+        h, w = x[0].shape[2], x[0].shape[3]
 
         out_road = []
         for i in range(seq_len):
-            x_i = x[i]
+            x_i = x[i*self.num_cam : i*self.num_cam + self.num_cam]
+            if isinstance(x_i, list):
+                x_i = torch.stack(x_i, dim=0)
+                x_i = torch.permute(x_i, (1,0,2,3,4))
+                x_i = torch.reshape(x_i, (batch_size*self.num_cam, 3, h, w))
+                # x_i = x_i.view(batch_size, self.num_cam, 3, h, w)
             with torch.no_grad():
                 x_i= (x_i - self.backbone.pixel_mean) / self.backbone.pixel_std
                 x_i = self.backbone.backbone(x_i)['res5']
-
-            x_feature_i = self.conv1(x_i)
+                down_h, down_w = x_i.shape[-2], x_i.shape[-1]
+                # x_i = torch.permute(x_i, (1,0,2,3,4))
+                x_i = x_i.view(batch_size, -1, down_h, down_w)
+            x_i = self.conv1(x_i)
+            x_feature_i = self.conv2(x_i)
             
 
             if self.road:
@@ -87,14 +95,14 @@ class CNNLSTM_maskformer(nn.Module):
                 x_feature_i = self.fusion(x_feature_i)
 
             x_feature_i = self.pool(x_feature_i)
-            x_feature_i = x_feature_i.view(batch_size*self.num_cam, 1, 256)
+            x_feature_i = x_feature_i.view(batch_size, 1, 256)
 
 
             if self.road:
                 x_road_i = self.pool(x_road_i)
                 x_road_i = x_road_i.view(batch_size*self.num_cam, 256)
                 out_road.append(x_road_i)
-                # out_road, hidden_road = self.en_road_lstm(x_road_i, hidden_road)
+                            # out_road, hidden_road = self.en_road_lstm(x_road_i, hidden_road)
             out, hidden = self.en_lstm(x_feature_i, hidden)
 
 
